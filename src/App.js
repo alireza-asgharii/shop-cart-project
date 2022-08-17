ef53557a988f0c0db3cef1013736137425cb0d24
Alireza <alirezwa03@gmail.com>
2022-08-17 21:14:29 +0430

    creat Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
 
+//Components
+import Footer from './components/Footer';
+import Main from './components/Main';
+import Navbar from './components/shared/Navbar';
+
 function App() {
   return (
    <>
+      <Navbar />
+      <Main />
+      <Footer />
    </>
   );
 }
