ace0aca1869b70596bfbf5516309684299878b34
Alireza <alirezwa03@gmail.com>
2022-08-20 08:22:02 +0430

    create Navbar component

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+//icons
+import {HiOutlineShoppingCart} from 'react-icons/hi';
+
+//styles
+import styles from '../../styles/navbar.module.scss';
+
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className={styles.container}>
+      <a href='#'>
+        <h1>My Shop</h1>
+      </a>
+      <a className={styles.cartIcon} href='#'>
+        <HiOutlineShoppingCart className={styles.shopIcon} />
+        <span className={styles.number}>0</span>
+      </a>
     </div>
   );
 };
