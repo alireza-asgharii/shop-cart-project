446b6e065fe157032b1640cf0dac561f3d0b5c63
Alireza <alirezwa03@gmail.com>
2022-08-20 12:57:42 +0430

    Create Detalis Component

---
+++ src/components/shared/Detalis.js
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+
+//Context
+import { DataContextProvider } from "../../Context/ApiContextProvider";
+
+//Styles
+import styles from "../../styles/detalis.module.scss";
+
+const Details = () => {
+  const params = useParams()
+  const data = useContext(DataContextProvider);
+  const product = data[params.id - 1];
+  console.log(product);
+
+  if (data.length > 0) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <img src={product.image} alt="product" />
+        </div>
+        <div className={styles.textContainer}>
+          <h1>{product.title}</h1>
+          <p className={styles.description}>{product.description}</p>
+          <p className={styles.price}>
+            <span>category: </span>
+            {product.category}
+          </p>
+          <div>
+            <span>{product.price} $</span>
+            <Link to="/products">Back to shop</Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Details;
