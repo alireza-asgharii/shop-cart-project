7f744078e7cc32aa7755803e1d04ecdf3693deaf
Alireza <alirezwa03@gmail.com>
2022-08-20 11:42:41 +0430

    clear StrictMode

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://fakestoreapi.com';
+
+axios.interceptors.response.use((response) => {
+  return response.data
+}, (err) => {
+  return Promise.reject(err);
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
