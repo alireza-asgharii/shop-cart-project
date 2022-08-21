37d74d6f1efbc1e6555b0875911b4ccb8e51bc0e
Alireza <alirezwa03@gmail.com>
2022-08-21 11:15:09 +0430

    Update style Components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+
+//Components
 import Products from "./Products";
 import Detalis from "./shared/Detalis";
 
+//styles
+import styles from '../styles/main.module.scss';
+
 const Main = () => {
   return (
-    <>
+    <div className={styles.container}>
       <Routes>
         <Route  path="/products" element={<Products />} />
         <Route path="/products/:id" element={<Detalis />} />
         <Route path="/*" element={<Navigate to='/products' /> } />
       </Routes>
-    </>
+    </div>
   );
 };
 
